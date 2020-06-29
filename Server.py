98b2a5941978189afa51cfa0455d49280ce84e97
xianghw <fly305z102@gmail.com>
2020-06-29 17:24:25 +0800

    f

--- Server.py
+++ Server.py
@@ -544,15 +544,15 @@ def handle_message(event):
             if text == "登出":
                 reply_text = "您已成功登出"
 
-            if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-                message = TextSendMessage(reply_text)
-                line_bot_api.reply_message(event.reply_token, message)
-            
-            u.state = states.UNLOGIN
-            headers = {"Authorization":"Bearer l82Nfs2Ji9XdgljwOFqOvPFQfQCytjakXuH1R8GB5oncFlzOPehHqxoj4utnElFJJBKfw2SUt2n7SiX56GIeSJwGglKRr0iCv78QttD7IaXe0zwxt9evRrbHObpOEp8FYCyTmqagFJt651108NGjYQdB04t89/1O/w1cDnyilFU=","Content-Type":"application/json","Content-Type":"application/json"}
-            req = requests.request('POST', ' https://api.line.me/v2/bot/user/' + u.user_id + '/richmenu/' + 'richmenu-6b8167a5a521e96c320ca94ad954e6c6', 
-                       headers=headers)
-                       
+                if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+                    message = TextSendMessage(reply_text)
+                    line_bot_api.reply_message(event.reply_token, message)
+                
+                u.state = states.UNLOGIN
+                headers = {"Authorization":"Bearer l82Nfs2Ji9XdgljwOFqOvPFQfQCytjakXuH1R8GB5oncFlzOPehHqxoj4utnElFJJBKfw2SUt2n7SiX56GIeSJwGglKRr0iCv78QttD7IaXe0zwxt9evRrbHObpOEp8FYCyTmqagFJt651108NGjYQdB04t89/1O/w1cDnyilFU=","Content-Type":"application/json","Content-Type":"application/json"}
+                req = requests.request('POST', ' https://api.line.me/v2/bot/user/' + u.user_id + '/richmenu/' + 'richmenu-6b8167a5a521e96c320ca94ad954e6c6', 
+                        headers=headers)
+
         elif u.state == states.DIV :
             if (u.div_id != 0):
                 line_bot_api.push_message(
