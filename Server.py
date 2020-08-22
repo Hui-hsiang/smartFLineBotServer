5b4d0bd03124bfe72ca86f187350b604e728736f
Hui-hsiang <48705017+Hui-hsiang@users.noreply.github.com>
2020-08-22 21:03:03 +0800

    no message

--- Server.py
+++ Server.py
@@ -272,12 +272,12 @@ def comment_flex(name, img_url, rank, docs):
                 ]
         })
 
-
+    print (img_url)
     content = {
         "type": "bubble",
         "hero": {
         "type": "image",
-        "url": "https://i.imgur.com/Hz8f9N3.jpg",
+        "url": img_url,
         "size": "full",
         "aspectRatio": "20:13",
         "aspectMode": "cover",
@@ -471,7 +471,12 @@ def handle_post_message(event):
         s_doc = db.collection('sales').document('U2649922b5604a80e08b0f9dba91f9029').get().to_dict()
         score = s_doc['score'] / s_doc['serviceCount']
         docs = db.collection("comment").where('id','==', 'U2649922b5604a80e08b0f9dba91f9029').get()
-        content= comment_flex(score,comment)
+        content= comment_flex('jerry',line_bot_api.get_profile('U60d04b2a91c5b050242a42de2c1b1947').picture_url,score,docs)
+        line_bot_api.reply_message(event.reply_token, line_bot_api.reply_message(
+                    event.reply_token,
+                    FlexSendMessage('交易紀錄', content)
+                    )
+                )
        
     elif event.postback.data == 'commentjerry':
         
@@ -479,7 +484,7 @@ def handle_post_message(event):
         s_doc = db.collection('sales').document('U60d04b2a91c5b050242a42de2c1b1947').get().to_dict()
         score = s_doc['score'] / s_doc['serviceCount']
         docs = db.collection("comment").where('id','==', 'U60d04b2a91c5b050242a42de2c1b1947').get()
-        content= comment_flex('jerry',line_bot_api.get_profile('U60d04b2a91c5b050242a42de2c1b1947').picture_url,score,docs)
+        content= comment_flex('apple',line_bot_api.get_profile('U2649922b5604a80e08b0f9dba91f9029').picture_url,score,docs)
         line_bot_api.reply_message(event.reply_token, line_bot_api.reply_message(
                     event.reply_token,
                     FlexSendMessage('交易紀錄', content)
