b0bb92171c87bfd25302291dc63572ad8b2db013
Hui-hsiang <48705017+Hui-hsiang@users.noreply.github.com>
2020-11-12 23:46:07 +0800

    no message

--- Server.py
+++ Server.py
@@ -2340,14 +2340,10 @@ def handle_message(event):
                 if (u.div_id != 0):
                     line_bot_api.push_message(
                         u.div_id,
-                        TextMessage(
-                            text=text,
-                            quick_reply=QuickReply(
-                            items=[
-                                QuickReplyButton(
-                                    action = MessageAction(
-                                        label = '離開',
-                                        text = '離開'))])))
+                        text_message = TextSendMessage(text=text,
+                               quick_reply=QuickReply(items=[
+                                   QuickReplyButton(action=MessageAction(label="離開", text="離開"))
+                               ])))
 
     UserData_update(u,doc)
 
